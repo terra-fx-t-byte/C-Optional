ba33977ee4fb69bae42f14162856d8637eac01ff
terra-fx-t-byte <228866656+terra-fx-t-byte@users.noreply.github.com>
2026-03-28 12:32:48 +0500

    Add files via upload

---
+++ Optional.h
@@ -0,0 +1,39 @@
+#ifndef OPTIONAL_H
+#define OPTIONAL_H
+
+#define DEFINE_OPTION(NAME, TYPE)\
+    typedef enum {NAME##_None, NAME##_Some} NAME##_Tag; \
+    typedef struct {    \
+        NAME##_Tag tag; \
+        union { \
+            TYPE some;  \
+        } data; \
+    } NAME;\
+    static inline NAME NAME##_some(TYPE value) {\
+        NAME opt;   \
+        opt.tag = NAME##_Some;  \
+        opt.data.some = value;  \
+        return opt; \
+    }\
+    static inline NAME NAME##_none() {\
+        NAME opt;   \
+        opt.tag = NAME##_None;  \
+        return opt; \
+    }\
+    static inline int NAME##_is_some(NAME* opt) {\
+        return opt->tag == NAME##_Some; \
+    }\
+    static inline TYPE NAME##_unwrap(NAME* opt) {\
+        return opt->data.some;  \
+    }\
+    static inline TYPE NAME##_unwrap_or(NAME* opt, TYPE* default_value) {\
+        return NAME##_is_some(opt) ? opt->data.some : *default_value;   \
+    }\
+    static inline TYPE NAME##_take(NAME* opt) {\
+        TYPE value = opt->data.some;    \
+        opt->tag = NAME##_None; \
+        return value;   \
+    }\
+
+
+#endif
